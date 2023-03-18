0d8dc6f05990746da70259fea8f32f301ebeca75
k3ny-dg <dutton-gillespie@student.greenriver.edu>
2023-03-17 19:10:43 -0700

    Commit initial files.

---
+++ src/test/java/DiceTests.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+import org.objects.Dice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DiceTests
+{
+    @Test
+    public void constructorTest()
+    {
+        Dice die = new Dice(6, "blue");
+        assertAll(
+                () -> assertEquals(6, die.getSides()),
+                () -> assertEquals("blue", die.getColor())
+        );
+    }
+    @Test
+    public void rollTest()
+    {
+        Dice die = new Dice(20, "teal");
+
+        int value = die.roll();
+
+        assertTrue(value <= die.getSides() + 1);
+    }
+
+    @Test
+    public void rollManyTest()
+    {
+        Dice dice = new Dice(6, "white");
+
+        dice.rollMany(5);
+        
+    }
+
+}
